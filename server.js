ee39fc41211910c363d67fe586985873b2d03fd5
Christian Vuerings <vueringschristian@gmail.com>
2015-07-24 19:53:36 -0700

    Force SSL in production

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var koa = require('koa');
 var app = koa();
+var forceSSL = require('koa-force-ssl');
 
-// logger
+// Force SSL on all page
+console.log('app.env', app.env);
+if (app.env === 'production') {
+  app.use(forceSSL());
+}
+
+// Logger
 app.use(function *(next){
   var start = new Date;
   yield next;
@@ -9,6 +16,7 @@ app.use(function *(next){
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
+// Standard response
 app.use(function *(){
   this.body = 'Hello World';
 });
